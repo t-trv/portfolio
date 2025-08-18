592d926971413ebff3a25299cfc3fc51c8466bbf
trvv <trhuvan@gmail.com>
2025-08-19 01:17:02 +0700

    done

---
+++ js/music_player.js
@@ -0,0 +1,100 @@
+const musicImg = document.querySelector('.music__img'); 
+const musicTitle = document.querySelector('.music__title');
+const playPauseBtn = document.querySelector('.music__play-pause ');
+const musicPrev = document.querySelector('.music__prev');
+const musicNext = document.querySelector('.music__next');
+
+const trackList = [
+    {
+        name: 'Le face\' so poor',
+        src: './assets/sounds/khuon-mat-dang-thuong.mp3',
+        img_src: './assets/avt_1.jpg',
+    },
+]
+
+let sound;
+let soundId = null;
+let currentTrackIndex = 0;
+
+function loadTrack(index) {
+    if (sound) sound.unload();
+    const track = trackList[index];
+    if (!track) return;
+    // Load and insert track name and image
+    musicImg.style.backgroundImage = `url(${track.img_src})`;
+    musicTitle.textContent = track.name;
+    // Initial sound setup
+    sound = new Howl({
+        src: [track.src],
+        html5: true,
+        volume: 0.5,
+        onplay: function() {
+            playPauseBtn.classList.add('music__play-pause--playing');
+            updateProgress();
+        },
+        onend: () => nextTrack()
+    })
+    soundId = sound.play();
+}
+
+function nextTrack() {
+    currentTrackIndex = (currentTrackIndex + 1) % trackList.length;
+    loadTrack(currentTrackIndex);
+}
+
+function prevTrack() {
+    currentTrackIndex = (currentTrackIndex - 1 + trackList.length) % trackList.length;
+    loadTrack(currentTrackIndex);
+}
+
+playPauseBtn.onclick = function() {
+    if (sound.playing()) {
+        sound.pause();
+        playPauseBtn.classList.toggle('music__play-pause--playing');
+    } else {
+        sound.play();
+        playPauseBtn.classList.toggle('music__play-pause--playing');
+    }
+}
+
+musicPrev.onclick = function() {
+    prevTrack();
+}
+musicNext.onclick = function() {
+    nextTrack();
+}
+// Initial load of the first track
+loadTrack(currentTrackIndex);
+
+// Progress bar ==============================================================
+const progressBg = document.querySelector('.music__progress-bg');
+const progressSlide = document.querySelector('.music__progress-slide');
+const currentTime = document.querySelector('.music__time-progress');
+const durationTime = document.querySelector('.music__time-duration');
+
+function updateProgress() {
+    let m = Math.floor(sound.duration() / 60);
+    let s = Math.floor(sound.duration() % 60);
+    durationTime.textContent = `${m}:${s < 10 ? '0' + s : s}`;
+
+    step = () => {
+        if (sound.playing()) {
+            let current = sound.seek();
+            let percent = (current / sound.duration()) * 100;
+            progressSlide.style.width = percent + '%';
+            currentTime.textContent = `${Math.floor(current / 60)}:${Math.floor(current % 60) < 10 ? '0' + Math.floor(current % 60) : Math.floor(current % 60)}`;
+        }
+        requestAnimationFrame(step);
+    }
+
+    step();
+}
+
+
+
+
+
+
+
+
+
---
+++ js/typing.js
@@ -0,0 +1,8 @@
+const infor_typing = new Typed('#infor_typing', {
+    strings: ['Join my discord!', 'Follow me on GitHub!', 'Check out my portfolio!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+    showCursor: true
+})
